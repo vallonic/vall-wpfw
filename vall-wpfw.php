87802659704c8dde906cefb50f519b678201750d
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 15:58:39 +0200

    Initial commit

---
+++ vall-wpfw.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Vallonic WordPress Framework
+Description: Maakt het leven van Vallonians makkelijker
+Version:     0.0.1
+Author:      Vallonic
+Author URI:  http://www.vallonic.com
+*/
+
+?>
