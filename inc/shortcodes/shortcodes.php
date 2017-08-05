1c0f5e3861f2affe1024ffa6f94a91b37aff6e6e
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 18:34:43 +0200

    fix

--- inc/shortcodes/shortcodes.php
+++ inc/shortcodes/shortcodes.php
@@ -4,9 +4,12 @@ foreach(glob('*.shortcodes.php') as $file) {
      include $file;
 }
 // Shortcode nu registreren en daarna inladen
+add_shortcode( 'vallonic-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
+add_shortcode( 'vallonic', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
+/*
 function initShortcodes() {
   add_shortcode( 'vallonic-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
   add_shortcode( 'vallonic', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
 }
-add_action('init', 'initShortcodes');
+add_action('init', 'initShortcodes');*/
 ?>
