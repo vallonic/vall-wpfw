131232b8eb9396de193f302db3f62118815535c7
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 18:51:07 +0200

    v0.0.1.3-alpha0.3.12

--- inc/shortcodes/shortcodes.php
+++ inc/shortcodes/shortcodes.php
@@ -5,13 +5,6 @@ foreach(glob('*.shortcodes.php') as $file) {
 }
 
 // Shortcode nu registreren en daarna inladen
-add_shortcode( 'vallonic-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
-add_shortcode( 'vallonic', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
-
-/*
-function initShortcodes() {
-  add_shortcode( 'vallonic-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
-  add_shortcode( 'vallonic', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
-}
-add_action('init', 'initShortcodes');*/
+add_shortcode( 'vall-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
+add_shortcode( 'vall', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
 ?>
--- vall-wpfw.php
+++ vall-wpfw.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Vallonic WordPress Framework
 Description: Maakt het leven van Vallonians makkelijker
-Version:     v0.0.1.3-alpha0.3.11
+Version:     v0.0.1.3-alpha0.3.12
 Author:      Vallonic
 Author URI:  http://www.vallonic.com
 */
