6ba3829e0bf0fb47fa9ea3b5d9a496dc40556d97
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 18:56:22 +0200

    v0.0.1.3-alpha0.3.13

--- inc/functions/shortcodes.php
+++ inc/functions/shortcodes.php
@@ -10,22 +10,19 @@ function vallGroet() {
 
   if ( $Hour >= 5 && $Hour <= 11 ) {
     $greet = "Goedemorgen";
-    return $greet;
   } else if ( $Hour >= 12 && $Hour <= 18 ) {
     $greet = "Goedemiddag";
-    return $greet;
   } else if ( $Hour >= 19 || $Hour <= 0 ) {
     $greet = "Goedeavond";
-    return $greet;
   } else if ( $Hour >= 1 || $Hour <= 4) {
     $greet = "Goedenacht";
-    return $greet;
   }
-  //return $greet;
-  //return "hallo";
+  return $greet;
 }
 
 // Shortcode nu registreren en daarna inladen
-add_shortcode( 'vall-groet', 'miscShortcodeGoodMEN' ); //Registreert de shortcode: 'miscShortcodes_goodMEN' uit 'misc'
-add_shortcode( 'vall', 'miscShortcodeVallonic' ); //Registreert de shortcode: 'miscShortcodes_vallonic' uit 'misc'
+function loadShortcodes() {
+  add_shortcode( 'vall-groet', 'vallGroet' );
+  add_shortcode( 'vall', 'vall' );
+}
 ?>
