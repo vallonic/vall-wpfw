298e2b827a3746c55d52ee4a510c4b5d3e76676c
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 18:28:35 +0200

    Fix

--- inc/shortcodes/misc.shortcodes.php
+++ inc/shortcodes/misc.shortcodes.php
@@ -1,24 +1,29 @@
 <?php
 ## SHORTCODE: Vallonic.com url
-function miscShortcodes_vallonic() {
+function miscShortcodeVallonic() {
   return "http://vallonic.com";
 }
 
 
-function miscShortcodes_goodMEN() {
+function miscShortcodeGoodMEN() {
   $Hour = date('G');
   $greet = "";
 
   if ( $Hour >= 5 && $Hour <= 11 ) {
     $greet = "Goedemorgen";
+    return $greet;
   } else if ( $Hour >= 12 && $Hour <= 18 ) {
     $greet = "Goedemiddag";
+    return $greet;
   } else if ( $Hour >= 19 || $Hour <= 0 ) {
     $greet = "Goedeavond";
+    return $greet;
   } else if ( $Hour >= 1 || $Hour <= 4) {
     $greet = "Goedenacht";
+    return $greet;
   }
-  return $greet;
+  //return $greet;
+  //return "hallo";
 }
 
 ?>
