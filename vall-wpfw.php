d6c1160cdf99023c0bfaef936c71bfcdf08f2d96
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 15:58:39 +0200

    Update versienummer

---
+++ vall-wpfw.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Vallonic WordPress Framework
+Description: Maakt het leven van Vallonians makkelijker
+Version:     v0.0.1
+Author:      Vallonic
+Author URI:  http://www.vallonic.com
+*/
+?>
