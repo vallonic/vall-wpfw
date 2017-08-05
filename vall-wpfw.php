084a1df4e4a8b1daf5072c0e250fc1fbd948f5d2
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 16:19:07 +0200

    Updater class * Op basis van Git Release op Github

--- vall-wpfw.php
+++ vall-wpfw.php
@@ -6,4 +6,10 @@ Version:     v0.0.1
 Author:      Vallonic
 Author URI:  http://www.vallonic.com
 */
+
+// Updater class inladen
+require_once( 'updater.php' );
+if ( is_admin() ) {
+    new VallonicWPFWGitHubPluginUpdater( __FILE__, 'vallonic', "vall-wpfw" );
+}
 ?>
