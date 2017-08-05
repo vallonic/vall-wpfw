261eb275aa0332555b0beeaafcfe8a4b21ca6c9a
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-05 20:05:52 +0200

    v0.0.1.3-alpha0.4.1

--- vall-wpfw.php
+++ vall-wpfw.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Vallonic WordPress Framework
 Description: Maakt het leven van Vallonians makkelijker
-Version:     v0.0.1.3-alpha0.3.20
+Version:     v0.0.1.3-alpha0.4
 Author:      Vallonic
 Author URI:  http://www.vallonic.com
 */
@@ -13,7 +13,11 @@ if ( is_admin() ) {
     new VallonicWPFWGitHubPluginUpdater( __FILE__, 'vallonic', "vall-wpfw" );
 }
 
+// Shortcodes
 require_once( 'inc/functions/shortcodes.php' );
 loadShortcodes();
+
+// Protected pages
+require_once( 'inc/functions/protectedpages.php' );
 
 ?>
