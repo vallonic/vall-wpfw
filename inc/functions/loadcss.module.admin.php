293c496f49d817a5dbc011b31097018ab77941d4
Thierry Maasdam <thierrymaasdam@users.noreply.github.com>
2017-08-07 09:43:19 +0200

    Haalt nu wel goed admin css op

--- inc/functions/loadcss.module.admin.php
+++ inc/functions/loadcss.module.admin.php
@@ -1,9 +1,12 @@
 <?php
 function vallLoadCSS() {
   global $plugin_path;
+  global $plugin_url;
   if ( is_admin() ) {
-    foreach ( glob( $plugin_path . "assets/css/*.css" ) as $file ) {
-      echo '<link rel="stylesheet" type="text/css" href="'. $file . '">';
+    foreach ( glob( $plugin_path . "assets/css/*.admin.css" ) as $file ) {
+      // Folder path uit string halen en vervangen met site url
+      $fileurl =  str_replace($plugin_path, $plugin_url, $file);
+      echo '<link rel="stylesheet" type="text/css" href="'. $fileurl . '">';
     }
   } else {}
 }
